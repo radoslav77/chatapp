cccc4f32e122cea5fa27800094c931023dc81fdc
unknown <rado7711@yahoo.co.uk>
2021-07-07 19:47:49 +0100

    Added user login and registration

--- chat/chatapp/urls.py
+++ chat/chatapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 # urls path
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login_user, name='login_user'),
+    path('logout', views.logout_user, name='logout_user'),
+    path('register', views.register, name='register')
 ]
--- chat/chatapp/views.py
+++ chat/chatapp/views.py
@@ -1,7 +1,74 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import *
+from django.contrib.auth import login, logout, authenticate
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, HttpResponseNotFound
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'chatapp/index.html')
+    if request.user.is_authenticated:
+        user = request.user.username
+
+    return render(request, 'chatapp/index.html', {
+        'user': user
+    })
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+        # if not logged in the register
+    else:
+        if request.method == "POST":
+            form = registrationForm(request.POST or None)
+            # Check if form is valid
+            if form.is_valid():
+                user = form.save()
+
+                # Get user password
+                raw_password = form.cleaned_data.get('password1')
+
+                # authenticate user
+                user = authenticate(username=user.username,
+                                    password=raw_password)
+                # login user
+                login(request, user)
+                return redirect('index')
+        else:
+            form = registrationForm()
+        return render(request, 'chatapp/register.html', {'form': form})
+
+
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+        # if not logged in then log in
+    else:
+        if request.method == "POST":
+            username = request.POST['username']
+            password = request.POST['password']
+
+            # Check credential
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('index')
+                else:
+                    return render(request, 'chatapp/login.html', {'error': "Your account has been desaibled."})
+
+            else:
+                return render(request, 'chatapp/login.html', {'error': 'Invalid username or password. Try Again.'})
+
+        return render(request, 'chatapp/login.html')
+
+
+# logout user
+def logout_user(request):
+    if request.user.is_authenticated:
+
+        logout(request)
+        return redirect('login_user')
+    else:
+        return redirect('login_user')
